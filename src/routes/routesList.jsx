3ab5e4804120479067ad3e176a2f0419c6fe6e77
YousefAbdulati <yousefjoe330@gmail.com>
2025-05-11 17:38:13 +0300

    feat:Add Search on Movies

--- src/routes/routesList.jsx
+++ src/routes/routesList.jsx
@@ -1,9 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { Routes, Route } from "react-router";
 import { MoviesList } from "../pages/moviesList";
+import { MoviesSearch } from "../pages/moviesSearch"; // Make sure it's imported
+
 export default function RoutesList() {
   return (
     <Routes>
-      <Route path="/" element={<MoviesList />}></Route>
+      <Route path="/" element={<MoviesList />} />
+      <Route path="/search/:query" element={<MoviesSearch />} />
+      <Route path="/search/" element={<MoviesSearch />} /> 
     </Routes>
   );
 }
--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,2 @@
 import axios from "axios";
-export const axiosInstance = axios.create({baseURL: import.meta.env.VITE_APP_API_BASE_URL})
+export const axiosMovies = axios.create({baseURL: import.meta.env.VITE_APP_API_BASE_URL})
