509352bf888764237bfdf969361b2062c16c12f5
yomna00 <yomna0917@gmail.com>
2025-05-12 04:03:51 +0300

    fix favourite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const favorites = useSelector((state) => state.movieData.favorites);
+
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand><Link to="/">🎬 MovieApp</Link></Navbar.Brand>
         <Nav className="ms-auto d-flex align-items-center gap-3">
-        <Link to="/series" className="btn btn-outline-light">Series</Link>
-        <Link to="/favorites" className="btn btn-outline-light">Favorites</Link>
-        <Link to="/watching" className="btn btn-outline-light">Watching</Link>
+          <Link to="/series" className="btn btn-outline-light">Series</Link>
+          <Link to="/favorites" className="btn btn-outline-light position-relative">Favorites</Link>
+          <Link to="/watching" className="btn btn-outline-light">Watching</Link>
         </Nav>
       </Container>
     </Navbar>
